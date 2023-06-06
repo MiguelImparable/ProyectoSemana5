82e30bae3c6452f86097407a607cb9461428c4cb
Miguel Angel Diaz Delgado <sql.programador35@opa.com.co>
2023-06-06 16:17:44 -0500

    solo modelos y controladores

--- DAL/PersonaDAL.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Mvc;
-using ProyectoSemana5.Context;
-using ProyectoSemana5.DTO;
-namespace ProyectoSemana5.DAL;
-
-public class PersonaDAL
-{
-    public void Read() {}
-}
--- DTO/PersonaDTO.cs
+++
@@ -1,8 +0,0 @@
-namespace ProyectoSemana5.DTO;
-public class Persona
-{
-    public string? nombre { get; set; }
-    public long cedula { get; set; }
-    public string? celular { get; set; }
-    public string? direccion { get; set; }
-}
